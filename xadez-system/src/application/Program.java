7a404f808464fbf7e8cb8537fe6051cbcd479481
Wagner Nogueira <wbnogueira3@gmail.com>
2023-04-06 16:00:45 -0300

    Criação do arquivo Position.java

--- xadez-system/src/application/Program.java
+++ xadez-system/src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
 
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
 
-        System.out.println("Hello world!");
     }
 }
