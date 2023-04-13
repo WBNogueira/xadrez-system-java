0fab0e79856edc548d0f61aa7616a59f5f62032b
Wagner Nogueira <wbnogueira3@gmail.com>
2023-04-13 16:42:36 -0300

    Revert "Little improvement in board printing"
    This reverts commit c81b9a584228547cc2b3c45d32da9a7ca0d3e9a2.

--- xadez-system/src/chess/ChessException.java
+++ xadez-system/src/chess/ChessException.java
@@ -1,12 +1,11 @@
 package chess;
 
-import boardgame.BoardException;
-
 import java.io.Serial;
 
-public class ChessException extends BoardException {
+public class ChessException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
+
     public ChessException(String msg) {
         super(msg);
     }
