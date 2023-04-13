c81b9a584228547cc2b3c45d32da9a7ca0d3e9a2
Wagner Nogueira <wbnogueira3@gmail.com>
2023-04-13 16:29:46 -0300

    Little improvement in board printing

--- xadez-system/src/chess/ChessException.java
+++ xadez-system/src/chess/ChessException.java
@@ -1,11 +1,12 @@
 package chess;
 
+import boardgame.BoardException;
+
 import java.io.Serial;
 
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
     @Serial
     private static final long serialVersionUID = 1L;
-
     public ChessException(String msg) {
         super(msg);
     }
