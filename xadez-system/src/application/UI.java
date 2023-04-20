97eab40f13c613b1392a22a0bac1b4a99453b711
Wagner Nogueira <wbnogueira3@gmail.com>
2023-04-20 11:42:32 -0300

    Bishop

--- xadez-system/src/application/UI.java
+++ xadez-system/src/application/UI.java
@@ -71,6 +71,7 @@ public class UI {
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
+        System.out.println("  a b c d e f g h");
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
@@ -82,6 +83,7 @@ public class UI {
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+        System.out.println("  a b c d e f g h");
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
